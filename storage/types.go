79cc3232325b05d7a0599ec1320b220caaf8bdb1
Dave Williams <dave@dave.io>
2019-03-10 23:56:39 +0000

    use array of arrays for commands

--- storage/types.go
+++ storage/types.go
@@ -4,14 +4,14 @@ type App struct {
 	Notes string `json:"notes"`
 	UseFullEnvironment bool `json:"useFullEnvironment"`
 	DetectAsRoot bool `json:"updateAsRoot"`
-	DetectScript []string `json:"detectScript"`
+	DetectScript [][]string `json:"detectScript"`
 	// DetectMatch string `json:"detectMatch"`
 	PrepAsRoot bool `json:"updateAsRoot"`
-	PrepScript []string `json:"prepScript"`
+	PrepScript [][]string `json:"prepScript"`
 	UpdateAsRoot bool `json:"updateAsRoot"`
-	UpdateScript []string `json:"updateScript"`
+	UpdateScript [][]string `json:"updateScript"`
 	PostAsRoot bool `json:"updateAsRoot"`
-	PostScript []string `json:"updateScript"`
+	PostScript [][]string `json:"updateScript"`
 }
 
 type Conf struct {
